f203ff4d5d28eead1098905e72b365f7573adf57
Morten Kals <mKals@dhcp-206-87-132-19.ubcsecure.wireless.ubc.ca>
2016-06-03 15:48:19 -0700

    files removed

--- ENPH257lab.m
+++ ENPH257lab.m
@@ -40,14 +40,6 @@ while toc < duration
         writeDigitalPin(a,powerPin,pOff);
     end
     
-    plot(times,temps(:,1),'r-'); hold on
-    plot(times,temps(:,2),'b-'); 
-    plot(times,temps(:,3),'g-'); 
-    plot(times,temps(:,4),'k-'); 
-    plot(times,temps(:,5),'m-'); 
-    plot(times,temps(:,6),'c-'); 
-    xlabel('time (s)'); ylabel('temperature (C)');
-    hold off
   
     pause(delayTime); 
 end
