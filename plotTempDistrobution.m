0b0ea0f405b9449509c0c0f94f300eaffbea782d
Morten Kals <mKals@dhcp-206-87-132-19.ubcsecure.wireless.ubc.ca>
2016-06-03 17:00:16 -0700

    updates to plotting algorithm

--- plotTempDistrobution.m
+++ plotTempDistrobution.m
@@ -1,10 +1,4 @@
 function [ ] = plotTempDistrobution( temps, row )
 
-x = [299 228 152 76 5];
-
-plot(x,temps(row,1:5), 'ro'); hold on
-xlabel('Distance along rod (mm)');
-ylabel('Temperature (C)');
-hold off
 
 end
