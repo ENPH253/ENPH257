f0492e9d15d8173b166a9957191a72b8f4bd149a
Morten Kals <mKals@Mortens-MacBook-Pro.local>
2016-06-03 21:35:48 -0700

    updated graphing code

--- plotData.m
+++ plotData.m
@@ -1,5 +1,6 @@
 function [ ] = plotData( times, temps )
     
+    %%
     subplot(1,2,1);
     plot(times,temps(:,1),'r-'); hold on
     plot(times,temps(:,2),'b-'); 
@@ -7,18 +8,20 @@ function [ ] = plotData( times, temps )
     plot(times,temps(:,4),'k-'); 
     plot(times,temps(:,5),'m-'); 
     plot(times,temps(:,6),'c-'); 
-    xlabel('time (s)'); ylabel('temperature (C)');
+    xlabel('time (s)'); ylabel('temperature (C)'); hold off
     
-    x = [0.299 0.228 0.152 0.076 0.005];
-        
-    temp = temps(1000, 1:5);
-    
+    %% 
     subplot(1,2,2);
-    cs = spline(x,temp);
+
+    x = [0.299 0.228 0.152 0.076 0.005];
     xx = linspace(x(1), x(end) ,100);
+
+    subTemps = temps(end, 1:5);
+    
+    cs = spline(x,subTemps);
     
     plot(xx, ppval(cs,xx)); hold on
-    plot(x,temps(1000,1:5), 'ro');
+    plot(x,subTemps, 'ro');
     xlabel('Distance along rod (mm)');
     ylabel('Temperature (C)');
     hold off
